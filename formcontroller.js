beeec8219d121ea5f72fd4c6adb583c413893412
saroj08 <saroj.mallick08@gmail.com>
2017-02-13 17:27:56 +0530

    newly updated

--- formcontroller.js
+++ formcontroller.js
@@ -2,6 +2,9 @@ var bodyparser=require("body-parser");
 var urlencodedparser=bodyparser.urlencoded({extended:false});
 var mysql      = require('mysql');
 var path = require('path');
+var multer=require('multer');
+//var mltr=multer({ dest: './uploads/'}).single('File');
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -24,7 +27,26 @@ module.exports=function(app){
 // })
 
 app.post('/form',urlencodedparser,function(req, res) {
+  var storage =   multer.diskStorage({
+
+    destination: function (req, file, callback) {
+      console.log("*************************************************************************************");
+      callback(null, './public/images');
+    },
+  filename: function (req, file, callback) {
+   callback(null, file.originalname);
+  }
+  });
+  var upload = multer({ storage : storage}).single('File');
+  upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+      console.log("successful");
+    });
+
   var count = 0;
+  var img;
 console.log('req.body');
 console.log(req.body);
 connection.query('select * from new_table',function(err,rows){
@@ -32,30 +54,31 @@ connection.query('select * from new_table',function(err,rows){
     throw err;
   else{
     for( var i in rows)
-    {console.log("called");
+    {
     if(rows[i].email== req.body.uemail||rows[i].firstname== req.body.fname||rows[i].lastname== req.body.lname)
     {     count++;
     console.log("user already exist");
+    console.log(rows[i].email);
+    // console.log(rows[i]["image"].toString());
+    img=rows[i]["image"].toString();
+    console.log(img);
      //res.render("form",{title:"already registered"});
 
      //res.end();
     } }
      if(count==0){
-     connection.query("Insert into new_table (firstname,lastname,email) VALUES('"+req.body.fname+"','"+req.body.lname+"','"+req.body.uemail+"')",function(err, result,fields)
+     connection.query("Insert into new_table (firstname,lastname,email,image) VALUES('"+req.body.fname+"','"+req.body.lname+"','"+req.body.uemail+"','"+req.body.File+"')",function(err, result,fields)
      {
           if (err){
           throw err;}
           console.log("connection successful");
-          // var html="<h1>welcome to this page</h1>";
-          // res.write(html);
-          // res.sendFile(path.join(__dirname + '/welcome.html'));
-          // res.write('You sent the name "' + req.body.fname+'".\n'+"<br>");
-          // res.write('You sent the name "' + req.body.lname+'".\n'+"<br>");
-          // res.write('You sent the Email "' + req.body.uemail+'".\n'+"<br>");
 var d=req.body.fname;
-          res.render("welcome",{title:"successfullly registered"+""+d});
+var i=req.file.filename;
+console.log(i);
 
-          // connection.end();
+
+  // img=rows[39]["image"].toString();
+          res.render("welcome",{title:"successfullly registered"+"     "+d,image:"images/"+i});
           res.end();
         });
 
@@ -65,6 +88,7 @@ var d=req.body.fname;
       }
 
 }
+
 
 // res.render("welcome");
 
